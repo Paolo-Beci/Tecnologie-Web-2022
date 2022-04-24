64c2616810fb37db074ecd25c0df5691e57eab8f
emanuelefrisi <s1092866@studenti.univpm.it>
2022-04-24 22:36:45 +0200

    Ancore dinamiche con lo scroll

--- menu-script.js
+++ menu-script.js
@@ -13,11 +13,40 @@ menuLink.forEach((menuLi) => {
 });
 
 $(window).scroll(function(){
-    var header = $('header'),
-        scroll = $(window).scrollTop();
-  
-    if (scroll >= 200)
-        header.addClass('header-scroll');
-    else
-        header.removeClass('header-scroll');
+
+    headerScroll();
+
+    navLinksScroll();
+
   });
+
+  function headerScroll(){
+
+    var header = $('header');
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll > parseFloat(header.css('height')))
+        header.addClass('header-scroll header-scroll-anim');
+    else if (scroll < parseFloat(header.css('height')) - 20)
+        header.removeClass('header-anim header-scroll header-scroll-anim');
+
+  }
+
+function navLinksScroll(){
+
+    var scrollPos = $(window).scrollTop();
+
+    $('nav a').each(function () {
+
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if (refElement.position().top <= scrollPos + 100) {
+            $('header a').removeClass("active");
+            currLink.addClass("active");
+        }
+
+    });
+
+}
