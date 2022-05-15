f2527fd511f03df1c7728e8c948a1376ceb85f8d
Paolo <S1092712@studenti.univpm.it>
2022-05-15 15:06:42 +0200

    Filtri V1

--- Project/laraProject/resources/views/helpers/filtri.blade.php
+++ Project/laraProject/resources/views/helpers/filtri.blade.php
@@ -1,11 +1,54 @@
 <h1>Filtri ricerca</h1>
 <h2>Fascia di Prezzo</h2>
+    <input type="text" placeholder="100" id="min-prezzo">
+    <label for="min-prezzo">&#8364; Minimi</label>
+    <input type="text" placeholder="1000" id="max-prezzo">
+    <label for="max-prezzo">&#8364; Massimi</label>
 <h2>Periodo locazione</h2>
+    <input type="radio" name="gender" id="12">
+    <label for="12"> 12 mesi</label>
+    <input type="radio" name="gender" id="9">
+    <label for="9"> 9 mesi</label>
+    <input type="radio" name="gender" id="6">
+    <label for="6"> 6 mesi</label>
 <h2>Superficie</h2>
+    <input type="text" placeholder="10" id="min-mq">
+    <label for="min-mq">Mq minimi</label>
+    <input type="text" placeholder="1000" id="max-mq">
+    <label for="max-mq">Mq massimi</label>
 <h2>Numero di camere</h2>
+    <input type="text" id="num_camere" name="num_camere" value="1"><br>
 <h2>Genere ammesso</h2>
+    <input type="radio" name="gender" id="female">
+    <label for="female"> Femmine</label>
+    <input type="radio" name="gender" id="male">
+    <label for="male"> Maschi</label>
+    <input type="radio" name="gender" id="all">
+    <label for="all"> Tutti</label>
 <h2>Piano</h2>
+    <input type="text" id="num_piano" name="num_piano" value="1"><br>
 <h2>Num. posti letto appartamento</h2>
+    <input type="text" id="num_posti_letto" name="num_posti_letto" value="1"><br>
 <h2>Num. posti letto camera</h2>
+    <input type="text" id="num_posti_letto_camera" name="num_posti_letto_camera" value="1"><br>
 <h2>Servizi</h2>
+    <input type="checkbox" id="check1" name="check1">
+    <label for="check1"> Bagno</label><br>
+    <input type="checkbox" id="check2" name="check2">
+    <label for="check2"> Cucina</label><br>
+    <input type="checkbox" id="check3" name="check3">
+    <label for="check3"> Lavanderia   </label>
+    <input type="checkbox" id="check4" name="check4">
+    <label for="check4"> Ripostiglio</label><br>
+    <input type="checkbox" id="check5" name="check5">
+    <label for="check5"> Garage</label><br>
+    <input type="checkbox" id="check6" name="check6">
+    <label for="check6"> Giardino</label>
 <h2>Tipo posto letto</h2>
+    <input type="radio" name="tipo" id="singola">
+    <label for="singola"> Singola</label>
+    <input type="radio" name="tipo" id="doppia">
+    <label for="doppia"> Doppia</label>
+<h2></h2>
+
+<input type="button" onclick="alert('Hello World!')" value="Filtra" class="button-filtra">
