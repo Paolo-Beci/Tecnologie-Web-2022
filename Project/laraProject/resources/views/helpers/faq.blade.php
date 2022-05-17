6d2a1d9a9ddd9c7bd41e09673e839d3175a2585c
emanuelefrisi <s1092866@studenti.univpm.it>
2022-05-17 10:59:30 +0200

    Modificato il css e collegato l'helper FAQ

--- Project/laraProject/resources/views/helpers/faq.blade.php
+++ Project/laraProject/resources/views/helpers/faq.blade.php
@@ -1,18 +1,16 @@
-@isset($faq)
-    <section id="faq" class="faq">
-        <h2>Hai dubbi o difficoltà?
-            <br>
-            Non preoccuparti!
-        </h2>
-        <p>Qui di seguito puoi trovare le più frequenti domande che gli utenti ci fanno!
-        </p>
-        <div class="faq-container">
-            @foreach ($faq as $singleFaq)
-                <article class="q-a">
-                    <p class="question">{!!$singleFaq->domanda!!}</p>
-                    <p class="answer">{!!$singleFaq->risposta!!}</p>
-                </article>
-            @endforeach
-        </div>
-    </section>
-@endisset
+<section id="faq" class="faq">
+    <h2>Hai dubbi o difficoltà?
+        <br>
+        Non preoccuparti!
+    </h2>
+    <p>Qui di seguito puoi trovare le più frequenti domande che gli utenti ci fanno!
+    </p>
+    <div class="faq-container">
+        @foreach ($faq as $singleFaq)
+            <article class="q-a">
+                <p class="question">{!!$singleFaq->domanda!!}</p>
+                <p class="answer">{!!$singleFaq->risposta!!}</p>
+            </article>
+        @endforeach
+    </div>
+</section>
--- Project/laraProject/resources/views/layouts/content-public.blade.php
+++ Project/laraProject/resources/views/layouts/content-public.blade.php
@@ -124,21 +124,6 @@
     </div>
 </section>
 @isset($faq)
-    <section id="faq" class="faq">
-        <h2>Hai dubbi o difficoltà?
-            <br>
-            Non preoccuparti!
-        </h2>
-        <p>Qui di seguito puoi trovare le più frequenti domande che gli utenti ci fanno!
-        </p>
-        <div class="faq-container">
-            @foreach ($faq as $singleFaq)
-                <article class="q-a">
-                    <p class="question">{!!$singleFaq->domanda!!}</p>
-                    <p class="answer">{!!$singleFaq->risposta!!}</p>
-                </article>  
-            @endforeach
-        </div>
-    </section>
+    @include('helpers/faq')
 @endisset
 @endsection
