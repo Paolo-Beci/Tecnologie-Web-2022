d023b24960a118e708edd13a055a36459d71bb02
emanuelefrisi <s1092866@studenti.univpm.it>
2022-05-17 16:50:38 +0200

    Aggiunta funzione di switch tra appartamento e posto letto

--- Project/laraProject/public/js/catalogo-script.js
+++ Project/laraProject/public/js/catalogo-script.js
@@ -1,3 +1,21 @@
-function onClickInserzione() {
-    // da fare
-}
+$('.select-button-appartamenti').on('click', function(){
+
+    $('.item-inserzione').each(function(){
+        if(this.hasAttribute('data-appartamento'))
+            $(this).addClass('visible');
+        else
+            $(this).removeClass('visible');
+    });
+
+});
+
+$('.select-button-posti-letto').on('click', function(){
+
+    $('.item-inserzione').each(function(){
+        if(this.hasAttribute('data-posto-letto'))
+            $(this).addClass('visible');
+        else
+            $(this).removeClass('visible');
+    });
+
+});
