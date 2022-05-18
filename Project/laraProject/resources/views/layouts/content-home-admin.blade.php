87eb7e696c8f3c503aede91f41e0e7fac4a77398
unknown <>
2022-05-18 20:43:57 +0200

    Modificata la sezione home admin

--- Project/laraProject/resources/views/layouts/content-home-admin.blade.php
+++ Project/laraProject/resources/views/layouts/content-home-admin.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
     <main id="content-home-admin" class="content-home-admin">
+        {{--sezione riassunto filtri--}}
         <section id="statistiche" class="stats-section">
-            <p class="titolo">Statistiche</p>
+            <h1 class="titolo">Statistiche</h1>
             <div class="stats-container">
                 <article class="stats">
-                    <p>Offerte di alloggio</p>
+                    <a href="#1">Offerte di alloggio</a>
                     <p>100</p>
                 </article>
                 <article class="stats">
-                    <p>Offerte di locazione</p>
+                    <a href="#2">Offerte di locazione</a>
                     <p>27</p>
                 </article>
                 <article class="stats">
-                    <p>Alloggi locati</p>
+                    <a href="#3">Alloggi locati</a>
                     <p>83</p>
                 </article>
+            </div>
+            <div id="1" class="loader">
+                <div class="ball"></div>
+                <div class="ball"></div>
+                <div class="ball"></div>
             </div>
         </section>
 
@@ -38,8 +44,30 @@
                         </select>
                     </label>
                 </div>
+                <div>
+                    <button class="filter_button" type="submit" onclick=alert('WorkInProgress')>Filtra</button>
+                </div>
+                <div class="results_container">
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/5.jpg')}}" alt="Alloggio 1">
+                            <h2>Foggia, Via Roma 4, interno 2 </h2>
+                        </div>
+                    </article>
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/6.jpg')}}" alt="Alloggio 1">
+                            <h2>Venezia, Via Marziale 2, interno 3 </h2>
+                        </div>
+                    </article>
+                </div>
             </div>
         </section>
+        <div id="2" class="loader">
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+        </div>
 
         {{--sezione offerte locazione--}}
         <section id="offerte_di_locazione" class="filter_section">
@@ -59,8 +87,30 @@
                         </select>
                     </label>
                 </div>
+                <div>
+                    <button class="filter_button" type="submit" onclick=alert('WorkInProgress')>Filtra</button>
+                </div>
+                <div class="results_container">
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/1.jpg')}}" alt="Alloggio 1">
+                            <h2>Milano, Via Giuseppe Rossi 2, interno 1 </h2>
+                        </div>
+                    </article>
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/2.jpg')}}" alt="Alloggio 1">
+                            <h2>Firenze, Via Dante Alighieri 18, interno 3 </h2>
+                        </div>
+                    </article>
+                </div>
             </div>
         </section>
+        <div id="3" class="loader">
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+        </div>
 
         {{--alloggi locati--}}
         <section id="alloggi_locati" class="filter_section">
@@ -79,6 +129,23 @@
                             <option>Posto letto
                         </select>
                     </label>
+                </div>
+                <div>
+                    <button class="filter_button" type="submit" onclick=alert('WorkInProgress')>Filtra</button>
+                </div>
+                <div class="results_container">
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/3.jpg')}}" alt="Alloggio 1">
+                            <h2>Ancona, Via Colleverde 18, interno 4 </h2>
+                        </div>
+                    </article>
+                    <article class="result">
+                        <div class="content">
+                            <img class="img_result" src="{{asset('images_case/4.jpg')}}" alt="Alloggio 1">
+                            <h2>Torino, Via della Mole 10, interno 8 </h2>
+                        </div>
+                    </article>
                 </div>
             </div>
         </section>
