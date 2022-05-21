0412908f0513404e31cbd6296782ee06b5ae7e51
IzziGiuseppe <s1094052@studenti.univpm.it>
2022-05-21 12:20:36 +0200

    Riorganizzazione rotte

--- Project/laraProject/routes/web.php
+++ Project/laraProject/routes/web.php
@@ -11,30 +11,29 @@
 |
 */
 
-// HOMEPAGE
+// PUBLIC
 Route::get('/', 'PublicController@showPublicHome')->name('homepage');
-
-Route::get('/home-locatore', 'LocatoreController@showLocatoreHome')->name('home-locatore');
-
-Route::get('/home-locatario', 'LocatarioController@showLocatarioHome')->name('home-locatario');
-
-Route::get('/home-admin', 'AdminController@showAdminHome')->name('home-admin');
 
 Route::get('/registrazione', function () {
     return view('registrazione-dati-personali');
 })->name('registrazione');
 
-// CATALOGO PUBBLICO
 Route::get('/catalogo', 'PublicController@showPublicCatalog')->name('catalogo');
 
 Route::get('/catalogo/appartamenti', 'PublicController@showPublicCatalogAppartamenti')->name('catalogo-appartamenti');
 
 Route::get('/catalogo/posti-letto', 'PublicController@showPublicCatalogPostiLetto')->name('catalogo-posti-letto');
 
-// CATALOGO LOCATORE
+//LOCATORE
+Route::get('/home-locatore', 'LocatoreController@showLocatoreHome')->name('home-locatore');
+
 Route::get('/catalogo-locatore', 'LocatoreController@showLocatoreCatalog')->name('catalogo-locatore');
 
-// CATALOGO LOCATARIO
+Route::get('/gestione_alloggi', 'LocatoreController@showLocatoreAlloggi')->name('gestione_alloggi');
+
+//LOCATARIO
+Route::get('/home-locatario', 'LocatarioController@showLocatarioHome')->name('home-locatario');
+
 Route::get('/catalogo-locatario', 'LocatarioController@showLocatarioCatalog')->name('catalogo-locatario');
 
 Route::get('/catalogo/dettagli-annuncio', function () {
@@ -42,12 +41,13 @@ Route::get('/catalogo/dettagli-annuncio', function () {
         ->with('user', 'locatario');
 })->name('dettagli-annuncio');
 
+//ADMIN
+Route::get('/home-admin', 'AdminController@showAdminHome')->name('home-admin');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-// GESTIONE FAQ
 Route::get('/gestione_faq', 'AdminController@showFaq')->name('gestione_faq');
 
-//GESTIONE ALLOGGI
-Route::get('/gestione_alloggi', 'LocatoreController@showLocatoreAlloggi')->name('gestione_alloggi');
+//Rotte legate all'autenticazione
+Auth::routes();
+
+
+
