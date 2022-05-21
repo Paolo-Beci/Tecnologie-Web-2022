53e37f60dabf89eae5bdea53ec4c3bcebb3e95a3
IzziGiuseppe <s1094052@studenti.univpm.it>
2022-05-21 11:51:06 +0200

    Aggiunta provvisoria della view "Gestione alloggi"

---
+++ Project/laraProject/resources/views/layouts/_navbar-gestione-alloggi.blade.php
@@ -0,0 +1,15 @@
+<ul class="menu">
+    <li>
+        <a class="active" href="{{ route('homepage') }}">Home</a>
+    </li>
+    <li>
+        <a class="" href="{{ route('homepage') }}">Inserisci annuncio</a>
+    </li>
+    <li>
+        <!-- TO DO -->
+        <button class="bottone_profilo" href="#profilo">
+            <img src="{{asset('images/user_icon.png')}}" alt="User Logo" width="10%" style="vertical-align:middle;horiz-align:left">
+            "Nome Utente"
+        </button>
+    </li>
+</ul>
