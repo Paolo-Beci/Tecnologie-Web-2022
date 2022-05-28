eab1d9da071c245954a3748e71ef58572de261cc
domenicolaporta00 <s1095492@studenti.univpm.it>
2022-05-28 19:20:50 +0200

    Modificati model, controller e view admin con aggiunta di nuove funzionalità: adesso alcuni dati della home page admin vengono prelevati dal db

--- laraProj5/app/Models/Admin.php
+++ laraProj5/app/Models/Admin.php
@@ -15,11 +15,39 @@ class Admin {
         return Faq::find($id);
     }
 
+    //metodo per ritornare il numero di offerte di alloggio
+    public function getOfferteAlloggio(){
+        return Alloggio::count();
+    }
+
+    //metodo per ritornare tutti gli alloggi per visualizzarli in stats (no paginate per ora)
+    public function getStatsAlloggi(){
+        return Alloggio::all();
+    }
+
+    //metodo per tornare un'array di alloggi in base alla tipologia in stats (no paginate per ora)
+    public function getStatsAlloggioByTip($tipologia){
+        return Alloggio::where('tipologia', $tipologia);
+    }
+
+    //da mettere la funzione per le foto
+
+    //metodo per ritornare il numero di offerte di locazione
+    public function getOfferteLocazione(){
+
+    }
+
+    //metodo per ritornare il numero di alloggi allocati
+    public function getAlloggiAllocati(){
+
+    }
+
+    //metodo per ritornare tutti gli alloggi per visualizzarli in catalogo (si paginate)
     public function getAlloggi(){
         return Alloggio::paginate(3);
     }
 
-    //metodo per tornare un'array di alloggi in base alla tipologia
+    //metodo per tornare un'array di alloggi in base alla tipologia in catalogo (si paginate)
     public function getAlloggioByTip($tipologia){
         return Alloggio::where('tipologia', $tipologia)->paginate(3);
     }
