568b11228e82ab0772ef3e082c0792d4e9e6a1cb
Paolo <S1092712@studenti.univpm.it>
2022-06-09 10:45:30 +0200

    fix rules

--- laraProj5/app/Http/Requests/UpdateProfileRequest.php
+++ laraProj5/app/Http/Requests/UpdateProfileRequest.php
@@ -34,7 +34,7 @@ class UpdateProfileRequest extends FormRequest {
             'cf' => ['required', 'alpha_num', 'min:16', 'max:16'],
             'city' => ['required', 'string', 'regex:/^[\pL\s\-]+$/u', 'max:255'],
             'street' => ['required', 'string', 'max:255'],
-            'house-number' => ['required', 'numeric', 'max:10'],
+            'house-number' => ['required', 'numeric', 'max:1000'],
             'cap' => ['required', 'string', 'regex:/^([0-9]{5})/u', 'max:5'],
             'email' => ['nullable', 'regex:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', 'max:255'],
             'telephone' => ['nullable', 'regex:/^(\+[0-9]{2}\s?)?[0-9]{3}\s?[0-9]{3}\s?[0-9]{4}$/u', 'max:10']
--- laraProj5/resources/views/helpers/alloggio.blade.php
+++ laraProj5/resources/views/helpers/alloggio.blade.php
@@ -6,17 +6,7 @@
     </div>
 @endcan
 
-<!-- Accesso dettagli alloggio in gestione alloggi di locatore -->
-@if(Route::current()->getName() == 'gestione-alloggi')
-@can('isLocatore')
-    <div class="alloggio" data-href="{{route('dettagli-alloggio-locatore', [$alloggio->id_alloggio, $alloggio->tipologia])}}">
-        @include('helpers/carta-alloggio-catalogo')
-    </div>
-@endcan
-@endif
-
 <!-- Popup di errore se non autorizzato a visualizzare i dettagli -->
-@if(Route::current()->getName() != 'gestione-alloggi')
 @cannot('isLocatario')
     <div class="alloggio" id="accedi" data-popup-caller>
         @include('helpers/carta-alloggio-catalogo')
